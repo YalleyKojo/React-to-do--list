a6b18d76b6d2c37533321a2438c0c098ab58e3ef
YalleyKojo <yalleyabraham@yahoo.com>
2022-03-29 05:21:49 +0800

    second commit

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header>
-            <h1>Whatever List abeg</h1>
+            <h1>To Do List</h1>
       </header>
     
   )
